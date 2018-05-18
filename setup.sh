1975fdde44e82d7877b5fa37cf80d25ee8a78a37
Huevos y Bacon <Huevos-y-Bacon@users.noreply.github.com>
2018-05-18 20:03:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+
 BIN=$(systemd-path user-binaries)
 echo "Copying the following files to $BIN:"
 for A in $(find . -type f -name aws\*); do echo $A; cp $A $BIN/;done
 for O in $(find . -type f -name okta\*); do echo $O; cp $O $BIN/;done
-
-
